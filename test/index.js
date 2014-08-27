1da390e8b7f90a1898f7c8d6e9512d65608409a7
Ilya Radchenko <ilya@burstcreations.com>
2014-08-27 11:13:31 -0400

    Add object test

--- test/index.js
+++ test/index.js
@@ -9,3 +9,14 @@ test('array, no tolerance', function (t) {
   t.same(result, [40.35, -71.20, 40.35, -71.20], 'bbox is same sw/ne if no tolerance');
   t.end();
 });
+
+test('lat/lng, no tolerance', function (t) {
+  var result1 = carp({ lat: 40.35, lng: -71.20 });
+  var result2 = carp({ latitude: 40.35, longitude: -71.20 });
+  var result3 = carp({ latitude: 40.35, lng: -71.20 });
+
+  t.same(result1, [40.35, -71.20, 40.35, -71.20], 'lat/lng - bbox is same sw/ne if no tolerance');
+  t.same(result2, [40.35, -71.20, 40.35, -71.20], 'latitude/longitude - bbox is same sw/ne if no tolerance');
+  t.same(result3, [40.35, -71.20, 40.35, -71.20], 'latitude/lng - bbox is same sw/ne if no tolerance');
+  t.end();
+});
